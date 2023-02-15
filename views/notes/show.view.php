bd62a117c40596645d47ff47bd15a68157fceb14
Kaunda21 <111141764+Kaunda21@users.noreply.github.com>
2023-02-15 16:52:38 +0300

    Add files via upload

---
+++ views/notes/show.view.php
@@ -0,0 +1,25 @@
+<?php
+
+require(base_path('views/partials/head.php'));
+
+require(base_path('views/partials/nav.php'));
+
+require(base_path('views/partials/banner.php'));
+?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p class="mb-6">
+            <a href="/notes" class="text-blue-500 underline">&larr; Go back</a>
+        </p>
+        <p><?= htmlspecialchars($note['body']) ?></p>
+
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="text" name="id" hidden value="<?= $note['id'] ?>">
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
+    </div>
+</main>
+
+<?php require(base_path('views/partials/foot.php')) ?>
