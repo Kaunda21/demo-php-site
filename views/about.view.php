bc30a8f1a427e4a354d21ed6905143ba5b661a6d
Kaunda21 <111141764+Kaunda21@users.noreply.github.com>
2023-02-15 16:50:58 +0300

    Add files via upload

---
+++ views/about.view.php
@@ -0,0 +1,16 @@
+<?php
+
+require('partials/head.php');
+
+require('partials/nav.php');
+
+require('partials/banner.php');
+?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Now you are in the about page</p>
+    </div>
+</main>
+
+<?php require('partials/foot.php') ?>
